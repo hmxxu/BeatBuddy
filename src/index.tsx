66c20b1fafdc98f601459c89846be69e3be3a027
pyenbu <pyenbu@uw.edu>
2023-04-22 13:39:26 -0700

    static song search bar added

--- src/index.tsx
+++ src/index.tsx
@@ -7,12 +7,14 @@ import './styles/index.css';
 import App from './App';
 import Navbar from './components/Navbar';
 import Filter from './components/Filter';
+import SearchBar from './components/SearchBar';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 // prop example: <App subject="Candiceeeeeee"/>
 root.render(
   <React.StrictMode>
     <Navbar/>
+    <SearchBar/>
     <Filter/>
   </React.StrictMode>
 );
