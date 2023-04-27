878e090877df315daf3dd661698251e8699081b2
pu098 <pu098@cs.washington.edu>
2023-04-27 15:38:38 -0700

    fixed accordion

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 // * index.tsx takes all of the components (Filter, Navbar, etc.) and renders
 // * them in 'root', which will appear in '/public/index.html (see line 31)'
 
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles/index.css';
 import Navbar from './components/Navbar';
@@ -9,16 +9,32 @@ import Filter from './components/Filter';
 import SearchBar from './components/SearchBar';
 import PlaylistReady from './components/PlaylistReady';
 import GeneratedPlaylist from './components/GeneratedPlaylist';
+import { id, qs } from './utils';
 
 const root = ReactDOM.createRoot(document.getElementById('root')!);
 // prop example: <App subject="Candiceeeeeee"/>
+
+
+function delayOverflow() {
+  let checkbox = id('customize-box') as HTMLInputElement;
+  if (checkbox.checked) {
+    console.log('checkbox is checked');
+    setTimeout(() => {
+      // id('two-filter').classList.add('overflow-visible');
+      id('two-filter').style.overflow = "visible";
+    }, 500);
+  } else {
+    id('two-filter').style.overflow = "hidden";
+  }
+}
+
 root.render(
   <React.StrictMode>
     <Navbar/>
     <SearchBar/>
     <div className="accordion">
       <label htmlFor="customize">Customize your playlist</label>
-      <input type="checkbox" name="accordion" id="first"/>
+      <input type="checkbox" name="accordion" id="customize-box" onClick={delayOverflow}/>
       {/* <h4>Use our filters to customize your recommended playlist.</h4> */}
       <div id="two-filter">
         <Filter key="language-filter" type="language-filter" />
@@ -30,3 +46,4 @@ root.render(
     <GeneratedPlaylist/>
   </React.StrictMode>
 );
+
