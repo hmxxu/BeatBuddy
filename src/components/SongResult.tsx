2545a1d5ccd20534c763d40c4ab6638a8dc97e9e
pu098 <pu098@cs.washington.edu>
2023-04-29 23:15:33 -0700

    first commit of the experiment branch for mobile mode

--- src/components/SongResult.tsx
+++ src/components/SongResult.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 // temp -- testing purposes
 import minami from './../images/minami.jpg';
+import play_btn from './../images/play-btn.png';
 
 function SongResult(props: any) {
   let artist = props.artist;
@@ -12,9 +13,16 @@ function SongResult(props: any) {
   return (
     <div className="song-result" onClick={props.onClick}>
       <img src={minami} alt="album cover"></img>
-      <p>{artist}</p>
-      <p>{title}</p>
-      <p>{genre}</p>
+      <div className="song-container-mobile">
+        <p>{title}</p>
+        <p>{artist}</p>
+      </div>
+      <p id="artist">{artist}</p>
+      <p id="title">{title}</p>
+      <p id="genre">{genre}</p>
+      <div className="play-btn-container">
+        <img src={play_btn} className="play-btn" alt="an icon of a play button"></img>
+      </div>
     </div>
   )
 };
