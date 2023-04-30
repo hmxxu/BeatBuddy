2d0d0fdf69b81e3ad3c8e62b41f53f0df55ae328
pu098 <pu098@cs.washington.edu>
2023-04-30 11:12:59 -0700

    working mobile mode

--- src/components/SongResult.tsx
+++ src/components/SongResult.tsx
@@ -11,17 +11,25 @@ function SongResult(props: any) {
 
   // onClick will call the function in SearchBar
   return (
-    <div className="song-result" onClick={props.onClick}>
-      <img src={minami} alt="album cover"></img>
-      <div className="song-container-mobile">
-        <p>{title}</p>
-        <p>{artist}</p>
+    <div className="song-result-container">
+
+      {/* The code below is for desktop */}
+      <div className="song-result" onClick={props.onClick}>
+        <img src={minami} alt="album cover" id="album-cover"></img>
+        <p id="artist">{artist}</p>
+        <p id="title">{title}</p>
+        <p id="genre">{genre}</p>
       </div>
-      <p id="artist">{artist}</p>
-      <p id="title">{title}</p>
-      <p id="genre">{genre}</p>
-      <div className="play-btn-container">
-        <img src={play_btn} className="play-btn" alt="an icon of a play button"></img>
+
+      {/* The code below is for mobile */}
+      <div className="song-result-mobile" onClick={props.onClick}>
+        <div className="song-container-mobile">
+          <p className="bold">{title}</p>
+          <p>{artist}</p>
+        </div>
+        <div className="play-btn-container-small">
+          <img src={play_btn} className="play-btn-small" alt="an icon of a play button"></img>
+        </div>
       </div>
     </div>
   )
