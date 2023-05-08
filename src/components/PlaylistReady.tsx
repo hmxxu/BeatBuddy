ee05ce52b407bd2ff6e62bd5d56636812f4ab009
pu098 <pu098@cs.washington.edu>
2023-05-08 16:38:47 -0700

    testing the dummyrec api

--- src/components/PlaylistReady.tsx
+++ src/components/PlaylistReady.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import { returnDummyRec, returnSpotifyRec } from '../beatbuddy/src/APIFunctions/ReturnSongStats';
 
 function PlaylistReady() {
+
+  async function generateRec() {
+    let limit: number = 20;
+    let artists_seed: string[] = ['0PHf0oiic0xAnCrRuLTtHl'];
+    let genres_seed: string[] = ['j-pop'];
+    let tracks_seed: string[] = ['69aL4LJK092UFLmWtFeFFy'];
+
+    let spotify_artist_id = '0PHf0oiic0xAnCrRuLTtHl';
+    let data = await returnDummyRec(spotify_artist_id);
+    console.log(data);
+  }
+  // function generateRec(){console.log('h');}
+
   return(
     <div>
         <h2>That's it! Your playlist is now ready.</h2>
-        <button id="generate-playlist-btn">Generate my playlist</button>
+        <button id="generate-playlist-btn" onClick={generateRec}>Generate my playlist</button>
     </div>
   )
 };
