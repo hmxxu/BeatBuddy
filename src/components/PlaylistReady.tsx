6ec0b85798ea3c2b7b6f51e678891c8cf7543962
Pahn Yenbut <84034262+pyenbu@users.noreply.github.com>
2023-05-08 20:42:44 -0700

    Frontend create playlist restructure (#23)
    * restructure of react
    
    * added function generateRec
    
    * removed redudant code
    
    * can get song id from search now
    
    * broken mTag pushing to parent
    
    * more efforts to fix childToParent for filters
    
    * fixed data usestate issue
    
    ---------
    
    Co-authored-by: pu098 <pu098@cs.washington.edu>

--- src/components/PlaylistReady.tsx
+++ src/components/PlaylistReady.tsx
@@ -1,7 +1,53 @@
 import React from 'react';
-import { returnDummyRec, returnSpotifyRec } from '../beatbuddy/src/APIFunctions/ReturnSongStats';
+import GeneratedPlaylist from './GeneratedPlaylist';
+import SearchBar from './SearchBar';
+import Filter from './Filter';
+import { id, qs } from '../utils';
+import accordion_icon from '../images/accordion-close.png';
+import { returnDummyRec } from '../beatbuddy/src/APIFunctions/ReturnSongStats';
+import { useState } from 'react';
 
 function PlaylistReady() {
+
+  const [songId, setSongId] = useState("");
+  const [genreList, setGenreList] = useState([]);
+  const [decadeList, setDecadeList] = useState([]);
+
+  const getSongId = (childData : any) => {
+    setSongId(childData)
+  }
+
+  const getGenreArray = (selectedList: any, fullList : any) => {
+    if (selectedList.length > 0) { 
+      setGenreList(selectedList);
+    } else {
+      setGenreList(fullList);
+    }
+    console.log(selectedList);
+  }
+
+  const getDecadeArray = (selectedList : any, fullList: any) => {
+    if (selectedList.length > 0) { 
+      setDecadeList(selectedList);
+    } else {
+      setDecadeList(fullList);
+    }
+    console.log(selectedList);
+
+  }
+
+  function delayOverflow() {
+    let checkbox = id('customize-box') as HTMLInputElement;
+    if (checkbox.checked) {
+      console.log('checkbox is checked');
+      setTimeout(() => {
+        // id('two-filter').classList.add('overflow-visible');
+        id('two-filter').style.overflow = "visible";
+      }, 400);
+    } else {
+      id('two-filter').style.overflow = "hidden";
+    }
+  }
 
   async function generateRec() {
     let limit: number = 20;
@@ -13,12 +59,29 @@ function PlaylistReady() {
     let data = await returnDummyRec(spotify_artist_id);
     console.log(data);
   }
-  // function generateRec(){console.log('h');}
-
   return(
     <div>
-        <h2>That's it! Your playlist is now ready.</h2>
-        <button id="generate-playlist-btn" onClick={generateRec}>Generate my playlist</button>
+      <SearchBar childToParent={ getSongId }/>
+      <div className="accordion">
+      {/* <span className="customize-text h2 bold">Customize your playlist</span> */}
+      <input type="checkbox" name="accordion" id="customize-box" onClick={delayOverflow}/>
+      <label htmlFor="customize-box" className="customize-label h2 bold">
+        <span className="customize-text h2 bold">Customize your playlist</span>
+        <img src={accordion_icon} alt="accordion-close" className="accordion-icon"></img>
+      </label>
+      <h4>Use our filters to customize your recommended playlist.</h4>
+      <div id="two-filter">
+        <Filter content="Time Period" key="language-filter" type="language-filter" childToParent={getDecadeArray}/>
+        <Filter content="Genre" key="genre-filter" type="genre-filter" childToParent={getGenreArray}/>
+      </div>
+    </div>
+
+
+      <div>
+          <h2>That's it! Your playlist is now ready.</h2>
+          <button id="generate-playlist-btn">Generate my playlist</button>
+      </div>
+      <GeneratedPlaylist/>
     </div>
   )
 };
